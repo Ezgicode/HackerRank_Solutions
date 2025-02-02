977e074b07c8735eeb98587a2952f5811db4da4b
Ezgicode <127981314+Ezgicode@users.noreply.github.com>
2025-02-02 15:44:18 +0300

    Create Power _Mod_Power.py

---
+++ Easy/Power _Mod_Power.py
@@ -0,0 +1,11 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a= int(input())
+b= int(input())
+m= int(input())
+
+def islem(a,b,m):
+    print(pow(a,b))
+    print(pow(a,b,m))
+    
+islem(a,b,m)
+
